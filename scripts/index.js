e24357d466a81d4981c614c3f17ddabf5d9fcd05
Edik Litvinov <e.litvinov00@yandex.by>
2020-09-28 22:04:33 +0300

    fix: исправить разметку страницы исправить функционал popup

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,35 @@ function popupToggle() {
 
 buttonOpenPopup.addEventListener('click', popupToggle);
 buttonPopupClose.addEventListener('click', popupToggle);
+
+// Находим форму в DOM
+let formElement =  document.querySelector('.popup__form');
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                        // Так мы можем определить свою логику отправки.
+                        // О том, как это делать, расскажем позже.
+
+    // Находим поля формы в DOM
+    let nameInput = document.querySelector('.popup__form-item_name').value;
+    let jobInput =  document.querySelector('.popup__form-item_job').value;
+
+    // Получите значение полей из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+    document.querySelector('.profile__title').textContent = nameInput;
+    document.querySelector('.profile__text').textContent = jobInput;
+    popupToggle();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+
