95413a304ca72f6318528bb633807e866838aaa6
Edik Litvinov <e.litvinov00@yandex.by>
2020-10-14 23:37:49 +0300

    style: поправить html

--- scripts/index.js
+++ scripts/index.js
@@ -1,71 +1,61 @@
-console.log('Hello, World!');
+console.log("Hello, World!");
 
-let popup = document.querySelector('.popup');
-let buttonPopupClose = document.querySelector('.popup__close-image');
-let buttonOpenPopup = document.querySelector('.profile__button');
-let formElement =  document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__form-item_name');
-let jobInput = document.querySelector('.popup__form-item_job');
-let profileTitle = document.querySelector('.profile__title');
-let profileText = document.querySelector('.profile__text');
+let popup = document.querySelector(".popup");
+let buttonPopupClose = document.querySelector(".popup__close-image");
+let buttonOpenPopup = document.querySelector(".profile__button");
+let formElement = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__form-item_name");
+let jobInput = document.querySelector(".popup__form-item_job");
+let profileTitle = document.querySelector(".profile__title");
+let profileText = document.querySelector(".profile__text");
 
 //Template
-let templatePhoto = document.querySelector('#template-photo').content;
+let templatePhoto = document.querySelector("#template-photo").content;
 
 // Форма Картинок
-let photoList = document.querySelector('.photo__list');
-let buttonAddImage = document.querySelector('.profile__button-add');
-let popupGallery = document.querySelector('.popup_gallery');
-let formImage = document.querySelector('.popup__form_gallery');
-let inputTitle = document.querySelector('.popup__form-item_title');
-let inputImage = document.querySelector('.popup__form-item_image');
-let photoTitle = document.querySelector('.photo__title');
-let photoItemImage = document.querySelector('.photo__item-image');
+let photoList = document.querySelector(".photo__list");
+let buttonAddImage = document.querySelector(".profile__button-add");
+let popupGallery = document.querySelector(".popup_gallery");
+let formImage = document.querySelector(".popup__form_gallery");
+let inputTitle = document.querySelector(".popup__form-item_title");
+let inputImage = document.querySelector(".popup__form-item_image");
+let photoTitle = document.querySelector(".photo__title");
+let photoItemImage = document.querySelector(".photo__item-image");
 // Попап картинок
-let popupPhotoScale = document.querySelector('.popup_photo-scale');
-let popupGalleryClose = document.querySelector('.popup__close-image_gallery');
+let popupPhotoScale = document.querySelector(".popup_photo-scale");
+let popupGalleryClose = document.querySelector(".popup__close-image_gallery");
 // let popupImageScale = document.querySelector('.popup__image-scale');
-let closeScale = document.querySelector('.popup__close-image_scale');
-
-
-
+let closeScale = document.querySelector(".popup__close-image_scale");
 
 const initialCards = [
   {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
   },
   {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
   },
   {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
   },
   {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
   },
   {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
   },
   {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
 ];
 
-
-// const newMitialCards = nitialCards.map(function(photo) {
-//   let templatePhoto = document.querySelector('.template-photo').content;
-//   let cloneTemplate = templatePhoto.cloneNode(true);
-
-// })
-
 const renderCards = () => {
- const cards = initialCards.map(element => {
+  const cards = initialCards.map((element) => {
     return getCard(element);
   });
 
@@ -75,93 +65,89 @@ const renderCards = () => {
 const getCard = (items) => {
   const card = templatePhoto.cloneNode(true);
   console.log(card);
-    card.querySelector('.photo__title').textContent = items.name;
-    card.querySelector('.photo__item-image').src = items.link;
-    card.querySelector('.photo__item-image').addEventListener('click', () => {
-    // const evtImage = evt.target;
-    popupPhotoScale.classList.toggle('popup_opened');
-    popupPhotoScale.querySelector('.popup__image-scale').src = items.link;
-    popupPhotoScale.querySelector('.popup__text').textContent = items.name;
+  card.querySelector(".photo__title").textContent = items.name;
+  card.querySelector(".photo__item-image").src = items.link;
+  card.querySelector(".photo__item-image").addEventListener("click", () => {
+    popupPhotoScale.classList.toggle("popup_opened");
+    popupPhotoScale.querySelector(".popup__image-scale").src = items.link;
+    popupPhotoScale.querySelector(".popup__text").textContent = items.name;
   });
-  card.querySelector('.photo__delete-button').innerHTML = `<img src="./images/DeleteIcon.svg" alt="Картинка удаления">`;
-  card.querySelector('.photo__delete-button').addEventListener('click', handlerRemove);
-  card.querySelector('.photo__button').addEventListener('click', (evt) => {
+  card.querySelector(".photo__delete-button").innerHTML = `<img src="./images/DeleteIcon.svg" alt="Картинка удаления">`;
+  card.querySelector(".photo__delete-button").addEventListener("click", handlerRemove);
+  card.querySelector(".photo__button").addEventListener("click", (evt) => {
     const evtTarget = evt.target;
-    evtTarget.classList.toggle('photo__button_active');
-    console.log(evtTarget)
+    evtTarget.classList.toggle("photo__button_active");
+    // console.log(evtTarget)
   });
 
   return card;
 };
 
-function formImageSubmitHandler (evt) {
+function formImageSubmitHandler(evt) {
   evt.preventDefault();
   const newItem = getCard({
     name: inputTitle.value,
-    link: inputImage.value
+    link: inputImage.value,
   });
   photoList.prepend(newItem);
-  inputTitle.value = '';
-  inputImage.value = '';
+  inputTitle.value = "";
+  inputImage.value = "";
   popupToggleImage();
-};
-
-const handlerRemove = (evt) => {
-  evt.target.closest('.photo__item').remove();
 }
 
+const handlerRemove = (evt) => {
+  evt.target.closest(".photo__item").remove();
+};
+
 function popupToggle() {
-  if (popup.classList.contains('popup_opened') !== true) {
+  if (popup.classList.contains("popup_opened") !== true) {
     nameInput.value = profileTitle.textContent;
     jobInput.value = profileText.textContent;
   }
-  popup.classList.toggle('popup_opened');
-};
+  popup.classList.toggle("popup_opened");
+}
 
 function popupToggleImage() {
-  popupGallery.classList.toggle('popup_opened');
-};
+  popupGallery.classList.toggle("popup_opened");
+}
 
 function scaleClose() {
-  popupPhotoScale.classList.toggle('popup_opened');
-};
-
+  popupPhotoScale.classList.toggle("popup_opened");
+}
 
 // Находим форму в DOM
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                        // Так мы можем определить свою логику отправки.
-                        // О том, как это делать, расскажем позже.
+function formSubmitHandler(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
 
-    // Находим поля формы в DOM
+  // Находим поля формы в DOM
 
-    // Получите значение полей из свойства value
+  // Получите значение полей из свойства value
 
-    // Выберите элементы, куда должны быть вставлены значения полей
+  // Выберите элементы, куда должны быть вставлены значения полей
 
-    // Вставьте новые значения с помощью textContent
-    profileTitle.textContent = nameInput.value;
-    profileText.textContent = jobInput.value;
-    popupToggle();
+  // Вставьте новые значения с помощью textContent
+  profileTitle.textContent = nameInput.value;
+  profileText.textContent = jobInput.value;
+  popupToggle();
 }
-
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-buttonOpenPopup.addEventListener('click', popupToggle);
-buttonPopupClose.addEventListener('click', popupToggle);
-formElement.addEventListener('submit', formSubmitHandler);
+buttonOpenPopup.addEventListener("click", popupToggle);
+buttonPopupClose.addEventListener("click", popupToggle);
+formElement.addEventListener("submit", formSubmitHandler);
 
-buttonAddImage.addEventListener('click', popupToggleImage);
-popupGalleryClose.addEventListener('click', popupToggleImage);
-formImage.addEventListener('submit', formImageSubmitHandler);
-closeScale.addEventListener('click', scaleClose);
-
-
+buttonAddImage.addEventListener("click", popupToggleImage);
+popupGalleryClose.addEventListener("click", popupToggleImage);
+formImage.addEventListener("submit", formImageSubmitHandler);
+closeScale.addEventListener("click", scaleClose);
 
 renderCards();
+
 
 
 
